054a61dbf50787a19fd584cb8995cf6f4870247a
pranga82 <pranga82@gmail.com>
2024-01-31 18:06:17 +0530

    New Class Added

---
+++ DDF_FrameWork/src/test/java/commonFunctions/Demo.java
@@ -0,0 +1,16 @@
+package commonFunctions;
+
+public class Demo {
+	public void add()
+	{
+		int a=65654,b=7898,c;
+		c=a+b;
+		System.out.println(c);
+	}
+	public void div()
+	{
+		int a=5465,b=66,c;
+		c=a/b;
+		System.out.println(c);
+	}
+}
